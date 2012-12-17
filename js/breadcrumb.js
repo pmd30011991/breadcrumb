43107393bd8a22b208545cd91ff16beb672e61b5
Phung Minh Duy <pmd.30011991@gmail.com>
2012-12-17 09:19:53 +0700

    init trunk

---
+++ js/breadcrumb.js
@@ -0,0 +1,8 @@
+$(function(){
+		$('div.breadcrumb ul li:nth-child(1)').css('z-index','6');
+		$('div.breadcrumb ul li:nth-child(2)').css('z-index','5');
+		$('div.breadcrumb ul li:nth-child(3)').css('z-index','4');
+		$('div.breadcrumb ul li:nth-child(4)').css('z-index','3');
+		$('div.breadcrumb ul li:nth-child(5)').css('z-index','2');
+		$('div.breadcrumb ul li:nth-child(6)').css('z-index','1');
+	});
